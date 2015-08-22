c23c3a534dda7e418834f4d33d1123a295810e18
baidonghui <baidonghui@live.cn>
2015-08-22 17:18:07 +0800

    150822

--- js/es-homegrid.js
+++ js/es-homegrid.js
@@ -1,1 +1,360 @@
-var webs=[{"n":"默认","g":"navmr","web":[{"n":"百度","u":"https://www.baidu.com","self":true,"hide":true},{"n":"谷歌","u":"http://duliziyou.com","self":true,"hide":true},{"n":"谷歌学术","u":"https://scholar.google.com"},{"n":"百度学术","u":"https://xueshu.baidu.com"},{"n":"新浪微博","u":"http://weibo.com","tile":"wb"},{"n":"百度贴吧","u":"http://tieba.baidu.com"},{"n":"燕山大学吧","u":"http://tieba.baidu.com/f?kw=%D1%E0%C9%BD%B4%F3%D1%A7&fr=index"},{"n":"知乎","u":"http://www.zhihu.com"}]},{"n":"翻译·论文","g":"navfy","web":[{"n":"Google翻译","u":"http://translate.google.cn/"},{"n":"有道翻译","u":"http://fanyi.youdao.com/"},{"n":"必应翻译","u":"http://www.bing.com/translator/"},{"n":"中国知网","u":"http://epub.cnki.net/kns/default.htm"},{"n":"维普期刊","u":"http://lib.cqvip.com/"},{"n":">>更多数据库","u":"http://library.ysu.edu.cn/sourceRight.aspx?caid=0&title=&parentID="}]},{"n":"燕大","g":"navyd","web":[{"n":"图书馆","u":"http://library.ysu.edu.cn","tile":"tsg"},{"n":"燕大新闻","u":"http://news.ysu.edu.cn/"},{"n":"本科生教务系统","u":"http://202.206.245.231/default2.aspx"},{"n":"里仁学院教务系统","u":"http://211.81.249.99/default2.aspx"},{"n":"研究生教务系统","u":"http://202.206.245.111/"},{"n":"校园直播","u":"http://yit.ysu.edu.cn/dmtfw/dszb/CCTV1.htm","tile":"zb"},{"n":"博云资料","u":"http://202.206.242.26:88/poweb/index.jsp"},{"n":">>燕大主页","u":"http://www.ysu.edu.cn"}]},{"n":"视频","g":"navsp","web":[{"n":"优酷","u":"http://www.youku.com/"},{"n":"爱奇艺","u":"http://www.iqiyi.com/"},{"n":"腾讯视频","u":"http://v.qq.com/"},{"n":"土豆","u":"http://www.tudou.com/"},{"n":"乐视","u":"http://www.letv.com/"},{"n":"哔哩哔哩","u":"http://www.bilibili.tv/"}]},{"n":"公开课","g":"navgkk","web":[{"n":"网易公开课","u":"http://open.163.com/"},{"n":"MOOC学院","u":"http://mooc.guokr.com/"},{"n":"Coursera","u":"https://www.coursera.org/","tile":"ca"},{"n":"edX","u":"https://www.edx.org/"},{"n":"Stanford Online","u":"http://online.stanford.edu/"},{"n":"优课联盟","u":"http://ysdx.benke.chaoxing.com/"},{"n":"极客学院","u":"http://www.jikexueyuan.com/"}]},{"n":"门户·新闻","g":"navmx","web":[{"n":"新浪门户","u":"http://www.sina.com.cn/"},{"n":"搜狐门户","u":"http://www.sohu.com/"},{"n":"腾讯门户","u":"http://www.qq.com/"},{"n":"网易门户","u":"http://www.163.com/"},{"n":"新浪新闻","u":"http://news.sina.com.cn/"},{"n":"凤凰新闻","u":"http://news.ifeng.com/"}]},{"n":"生活","g":"navsh","web":[{"n":"4399小游戏","u":"http://www.4399.com/"},{"n":"12306","u":"http://www.12306.cn/"},{"n":"工商银行","u":"http://www.icbc.com.cn/"},{"n":"农业银行","u":"http://www.abchina.com/","tile":"nh"},{"n":"中国移动","u":"http://www.10086.cn/"},{"n":"中国电信","u":"http://www.189.cn/"},{"n":"中国联通","u":"http://www.10010.com/"}]},{"n":"购物","g":"navgw","web":[{"n":"淘宝","u":"http://www.taobao.com",},{"n":"淘宝特卖","u":"http://temai.taobao.com/event9776.htm?pid=mm_34619426_10544719_34746681","tile":"tb"},{"n":"天猫精选","u":"http://s.click.taobao.com/JVTYq1y?pid=mm_34619426_10544719_34746681","tile":"tm"},{"n":"9块9包邮","u":"http://temai.taobao.com/event10323.htm?pid=mm_34619426_10544719_34746681"},{"n":"聚划算","u":"http://s.click.taobao.com/ZQscq1y?pid=mm_34619426_10544719_34746681"},{"n":"美团","u":"http://www.meituan.com/"},{"n":"百度糯米","u":"http://www.nuomi.com/"},{"n":"聚美优品","u":"http://www.jumei.com/"},{"n":"唯品会","u":"http://www.vip.com"},{"n":"京东商城","u":"http://www.jd.com"},{"n":"当当","u":"http://www.dangdang.com"},{"n":"亚马逊","u":"http://www.amazon.cn"},{"n":"蘑菇街","u":"http://www.mogujie.com"},{"n":">>试试手气","id":"trythis"}]}];function gengrid(){$("#webgrid").empty();$("#tile").empty();var rcols=parseInt($("#webgrid").width()/150);$.each(webs,function(n,value){var webgroup=value.g;$("#webgrid").append('<div id="'+webgroup+'0" class="row cells'+rcols+'"><span class="cell" align="center">'+value.n+'</span></div>');var webid=0;$.each(value.web,function(n,value){if(value.tile==undefined){if(value.hide==undefined){webid++;var rowid=parseInt(webid/rcols);if(webid%rcols==0){$("#"+webgroup+(rowid-1)).after('<div id="'+webgroup+rowid+'" class="row cells'+rcols+'"><span class="cell" align="center"></span></div>')}if(value.id==undefined){$("#"+webgroup+rowid).append('<div class="cell"><a href="'+value.u+'" target="_blank">'+value.n+'</a></div>')}else{$("#"+webgroup+rowid).append('<div class="cell"><a id="'+value.id+'" href="#">'+value.n+'</a></div>')}}}else{$("#tile").append(getTile(value.u,'img//'+value.tile+'.png',value.n))}})})};function getTile(url,img,text){return'<div rel="tile" class="tile fg-white" onclick="openTile(\''+url+'\')"><div class="tile-content slide-up"><div class="slide"><div class="image-container image-format-square" style="width: 100%;"><div class="frame" style="background-position: center; background-image: url('+img+'); background-size: auto; background-repeat: no-repeat;"></div></div></div><div class="slide-over op-cyan text-small padding10">'+text+'</div></div></div>'}function showWeather(){var info='';$.ajax({url:"http://api.openweathermap.org/data/2.5/forecast/daily?id=1797595&units=metric&cnt=2&lang=zh",type:'GET',dataType:'JSONP',success:function(data){$.each(data.list,function(n,value){info+='<span style="padding-left:20px">';info+=(n==0)?'今日':'明日';info+=value.weather[0].description+' '+value.temp.min+'~'+value.temp.max+'°C</span>'})$("#weather").append(info)}})}$(window).resize(function(){gengrid()});$(document).ready(function(){gengrid();$("#trythis").click(function(){var rnd=["http://i.83jie.com/stt/6iZtZGaVdA?pid=mm_34619426_10544719_34746681","http://s.click.taobao.com/kdydq1y?pid=mm_34619426_10544719_34746681","http://temai.taobao.com/event9776.htm?pid=mm_34619426_10544719_34746681","http://s.click.taobao.com/kmDZq1y?pid=mm_34619426_10544719_34746681","http://i.83jie.com/stt/UlPjORaVdA?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.12s12","http://i.83jie.com/stt/nLBRWqMXdA?pid=mm_34619426_10544719_34746681","http://s.click.taobao.com/VtmSJvx?pid=mm_34619426_10544719_34746681","http://temai.taobao.com?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.4s12","http://temai.taobao.com/event9769.htm?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.4s12","http://temai.taobao.com/event9776.htm?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.4s12","http://s.click.taobao.com/na5cq1y?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.4s12","http://temai.taobao.com/event10323.htm?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.5s12","http://temai.taobao.com/cheap.htm?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.5s12","http://ai.taobao.com/?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.6s12","http://ai.taobao.com/album/detail.htm?preview=true&albumId=31557&pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.8s12","http://s.click.taobao.com/lB7Yq1y?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.9s12","http://s.click.taobao.com/ZQscq1y?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.9s12","http://s.click.taobao.com/JLcXq1y?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.9s12","http://s.click.taobao.com/JVTYq1y?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.10s12","http://s.click.taobao.com/JVTYq1y?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.11s12"]var url=rnd[Math.floor(Math.random()*rnd.length)];window.open(url,"_blank")});showWeather()});
+var webs = [{
+	"n": "默认",
+	"g": "navmr",
+	"web": [{
+		"n": "百度",
+		"u": "https://www.baidu.com",
+		"self": true,
+		"hide": true
+	}, {
+		"n": "谷歌",
+		"u": "http://duliziyou.com",
+		"self": true,
+		"hide": true
+	}, {
+		"n": "谷歌学术",
+		"u": "https://scholar.google.com"
+	}, {
+		"n": "百度学术",
+		"u": "https://xueshu.baidu.com"
+	}, {
+		"n": "新浪微博",
+		"u": "http://weibo.com",
+		"tile": "wb"
+	}, {
+		"n": "百度贴吧",
+		"u": "http://tieba.baidu.com"
+	}, {
+		"n": "燕山大学吧",
+		"u": "http://tieba.baidu.com/f?kw=%D1%E0%C9%BD%B4%F3%D1%A7&fr=index"
+	}, {
+		"n": "知乎",
+		"u": "http://www.zhihu.com"
+	}]
+}, {
+	"n": "翻译·论文",
+	"g": "navfy",
+	"web": [{
+		"n": "Google翻译",
+		"u": "http://translate.google.cn/"
+	}, {
+		"n": "有道翻译",
+		"u": "http://fanyi.youdao.com/"
+	}, {
+		"n": "必应翻译",
+		"u": "http://www.bing.com/translator/"
+	}, {
+		"n": "中国知网",
+		"u": "http://epub.cnki.net/kns/default.htm"
+	}, {
+		"n": "维普期刊",
+		"u": "http://lib.cqvip.com/"
+	}, {
+		"n": ">>更多数据库",
+		"u": "http://library.ysu.edu.cn/sourceRight.aspx?caid=0&title=&parentID="
+	}]
+}, {
+	"n": "燕大",
+	"g": "navyd",
+	"web": [{
+		"n": "图书馆",
+		"u": "http://library.ysu.edu.cn",
+		"tile": "tsg"
+	}, {
+		"n": "燕大新闻",
+		"u": "http://news.ysu.edu.cn/"
+	}, {
+		"n": "本科生教务系统",
+		"u": "http://202.206.245.231/default2.aspx"
+	}, {
+		"n": "里仁学院教务系统",
+		"u": "http://211.81.249.99/default2.aspx"
+	}, {
+		"n": "研究生教务系统",
+		"u": "http://202.206.245.111/"
+	}, {
+		"n": "校园直播",
+		"u": "http://yit.ysu.edu.cn/dmtfw/dszb/CCTV1.htm",
+		"tile": "zb"
+	}, {
+		"n": "博云资料",
+		"u": "http://202.206.242.26:88/poweb/index.jsp"
+	}, {
+		"n": ">>燕大主页",
+		"u": "http://www.ysu.edu.cn"
+	}]
+}, {
+	"n": "视频",
+	"g": "navsp",
+	"web": [{
+		"n": "优酷",
+		"u": "http://www.youku.com/"
+	}, {
+		"n": "爱奇艺",
+		"u": "http://www.iqiyi.com/"
+	}, {
+		"n": "腾讯视频",
+		"u": "http://v.qq.com/"
+	}, {
+		"n": "土豆",
+		"u": "http://www.tudou.com/"
+	}, {
+		"n": "乐视",
+		"u": "http://www.letv.com/"
+	}, {
+		"n": "哔哩哔哩",
+		"u": "http://www.bilibili.tv/"
+	}]
+}, {
+	"n": "公开课",
+	"g": "navgkk",
+	"web": [{
+		"n": "网易公开课",
+		"u": "http://open.163.com/"
+	}, {
+		"n": "MOOC学院",
+		"u": "http://mooc.guokr.com/"
+	}, {
+		"n": "Coursera",
+		"u": "https://www.coursera.org/",
+		"tile": "ca"
+	}, {
+		"n": "edX",
+		"u": "https://www.edx.org/"
+	}, {
+		"n": "Stanford Online",
+		"u": "http://online.stanford.edu/"
+	}, {
+		"n": "优课联盟",
+		"u": "http://ysdx.benke.chaoxing.com/"
+	}, {
+		"n": "极客学院",
+		"u": "http://www.jikexueyuan.com/"
+	}]
+}, {
+	"n": "门户·新闻",
+	"g": "navmx",
+	"web": [{
+		"n": "新浪门户",
+		"u": "http://www.sina.com.cn/"
+	}, {
+		"n": "搜狐门户",
+		"u": "http://www.sohu.com/"
+	}, {
+		"n": "腾讯门户",
+		"u": "http://www.qq.com/"
+	}, {
+		"n": "网易门户",
+		"u": "http://www.163.com/"
+	}, {
+		"n": "新浪新闻",
+		"u": "http://news.sina.com.cn/"
+	}, {
+		"n": "凤凰新闻",
+		"u": "http://news.ifeng.com/"
+	}]
+}, {
+	"n": "生活",
+	"g": "navsh",
+	"web": [{
+		"n": "4399小游戏",
+		"u": "http://www.4399.com/"
+	}, {
+		"n": "12306",
+		"u": "http://www.12306.cn/"
+	}, {
+		"n": "工商银行",
+		"u": "http://www.icbc.com.cn/"
+	}, {
+		"n": "农业银行",
+		"u": "http://www.abchina.com/",
+		"tile": "nh"
+	}, {
+		"n": "中国移动",
+		"u": "http://www.10086.cn/"
+	}, {
+		"n": "中国电信",
+		"u": "http://www.189.cn/"
+	}, {
+		"n": "中国联通",
+		"u": "http://www.10010.com/"
+	}]
+}, {
+	"n": "购物",
+	"g": "navgw",
+	"web": [{
+		"n": "淘宝",
+		"u": "http://www.taobao.com",
+	}, {
+		"n": "淘宝特卖",
+		"u": "http://temai.taobao.com/event9776.htm?pid=mm_34619426_10544719_34746681",
+		"tile": "tb"
+	}, {
+		"n": "天猫精选",
+		"u": "http://s.click.taobao.com/JVTYq1y?pid=mm_34619426_10544719_34746681",
+		"tile": "tm"
+	}, {
+		"n": "9块9包邮",
+		"u": "http://temai.taobao.com/event10323.htm?pid=mm_34619426_10544719_34746681"
+	}, {
+		"n": "聚划算",
+		"u": "http://s.click.taobao.com/ZQscq1y?pid=mm_34619426_10544719_34746681"
+	}, {
+		"n": "美团",
+		"u": "http://www.meituan.com/"
+	}, {
+		"n": "百度糯米",
+		"u": "http://www.nuomi.com/"
+	}, {
+		"n": "聚美优品",
+		"u": "http://www.jumei.com/"
+	}, {
+		"n": "唯品会",
+		"u": "http://www.vip.com"
+	}, {
+		"n": "京东商城",
+		"u": "http://www.jd.com"
+	}, {
+		"n": "当当",
+		"u": "http://www.dangdang.com"
+	}, {
+		"n": "亚马逊",
+		"u": "http://www.amazon.cn"
+	}, {
+		"n": "蘑菇街",
+		"u": "http://www.mogujie.com"
+	}, {
+		"n": ">>试试手气",
+		"id": "trythis"
+	}]
+}];
+
+
+function gengrid() {
+	$("#webgrid").empty();
+	$("#tile").empty();
+
+	var rcols = parseInt($("#webgrid").width() / 150);
+	$.each(webs, function(n, value) {
+
+		var webgroup = value.g;
+		$("#webgrid").append('<div id="' + webgroup + '0" class="row cells' + rcols + '"><span class="cell" align="center">' + value.n + '</span></div>');
+
+		var webid = 0;
+		$.each(value.web, function(n, value) {
+			if (value.tile == undefined) {
+				if (value.hide == undefined) {
+					webid++;
+					var rowid = parseInt(webid / rcols);
+					if (webid % rcols == 0) {
+						$("#" + webgroup + (rowid - 1)).after('<div id="' + webgroup + rowid + '" class="row cells' + rcols + '"><span class="cell" align="center"></span></div>');
+						webid++;
+					}
+
+					if (value.id == undefined) {
+						$("#" + webgroup + rowid).append('<div class="cell"><a href="' + value.u + '" target="_blank">' + value.n + '</a></div>');
+					} else {
+						$("#" + webgroup + rowid).append('<div class="cell"><a id="' + value.id + '" href="#" onclick="_czc.push([\'_trackEvent\', \'good\', \'click\', \'\', \'\', \'trythis\']);trythisfun();">' + value.n + '</a></div>');
+					}
+
+				}
+			} else {
+				$("#tile").append(getTile(value.u, 'img//' + value.tile + '.png', value.n));
+			}
+		});
+	});
+};
+
+
+function getTile(url, img, text) {
+	return '<div rel="tile" class="tile fg-white" onclick="openTile(\'' + url + '\')"><div class="tile-content slide-up"><div class="slide"><div class="image-container image-format-square" style="width: 100%;"><div class="frame" style="background-position: center; background-image: url(' + img + '); background-size: auto; background-repeat: no-repeat;"></div></div></div><div class="slide-over op-cyan text-small padding10">' + text + '</div></div></div>'
+}
+
+
+function showWeather() {
+	var info = '';
+
+	$.ajax({
+		url: "http://api.openweathermap.org/data/2.5/forecast/daily?id=1797595&units=metric&cnt=2&lang=zh",
+		type: 'GET',
+		dataType: 'JSONP',
+		success: function(data) {
+			$.each(data.list, function(n, value) {
+				info += '<span style="padding-left:20px;float:right">';
+				info += (n == 0) ? '今日' : '明日';
+				info += value.weather[0].description + ' ' + value.temp.min + '~' + value.temp.max + '°C</span>';
+			})
+			$("#weather").append(info);
+		}
+	});
+}
+
+$(window).resize(function() {
+	gengrid();
+});
+$(document).ready(function() {
+	
+	showWeather();
+	
+	gengrid();
+
+	// $("#trythis").click(function() {
+		
+	// });
+
+});
+
+function trythisfun()
+{
+	var rnd = [ //他在推推推
+			"http://i.83jie.com/stt/6iZtZGaVdA?pid=mm_34619426_10544719_34746681", //头条：七夕美饰一秒变女神
+			"http://s.click.taobao.com/kdydq1y?pid=mm_34619426_10544719_34746681", //天猫美妆频道
+			"http://temai.taobao.com/event9776.htm?pid=mm_34619426_10544719_34746681", //淘宝特卖 精品疯抢季
+			"http://s.click.taobao.com/kmDZq1y?pid=mm_34619426_10544719_34746681", //天猫男装频道
+			"http://i.83jie.com/stt/UlPjORaVdA?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.12s12", //神团购丝巾来袭
+			//自媒体 自媒体必备
+			"http://i.83jie.com/stt/nLBRWqMXdA?pid=mm_34619426_10544719_34746681", //女神新装那一款最爱
+			//内容网站
+			"http://s.click.taobao.com/VtmSJvx?pid=mm_34619426_10544719_34746681", //天猫新风尚
+			//淘宝特卖PC
+			"http://temai.taobao.com?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.4s12",
+			//淘宝特卖一折清仓
+			"http://temai.taobao.com/event9769.htm?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.4s12",
+			//淘宝特卖精品疯抢季
+			"http://temai.taobao.com/event9776.htm?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.4s12",
+			//天猫女装频道
+			"http://s.click.taobao.com/na5cq1y?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.4s12",
+			//淘宝特卖9块9包邮
+			"http://temai.taobao.com/event10323.htm?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.5s12",
+			//淘宝特卖天天9.9PC
+			"http://temai.taobao.com/cheap.htm?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.5s12",
+
+			//爱淘宝PC
+			"http://ai.taobao.com/?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.6s12",
+
+			//天猫电器抢稀缺数码
+			"http://ai.taobao.com/album/detail.htm?preview=true&albumId=31557&pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.8s12",
+
+			//功能网站
+
+			//天猫超市
+			"http://s.click.taobao.com/lB7Yq1y?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.9s12",
+
+			//聚划算
+			"http://s.click.taobao.com/ZQscq1y?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.9s12",
+
+			//淘优惠
+			"http://s.click.taobao.com/JLcXq1y?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.9s12",
+
+			//天猫精选
+			"http://s.click.taobao.com/JVTYq1y?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.10s12",
+
+			//天猫精选值得选择
+			"http://s.click.taobao.com/JVTYq1y?pid=mm_34619426_10544719_34746681&scm=20140618.1.02030003.11s12"
+		]
+		
+		
+		
+		var url = rnd[Math.floor(Math.random() * rnd.length)];
+		window.open(url , "_blank");
+}
