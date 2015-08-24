f70b925c2aedc087404fdb2d4cd2058512a5a4b0
baidonghui <baidonghui@live.cn>
2015-08-24 11:39:27 +0800

    alpha_6

--- js/es-custom-home.js
+++ js/es-custom-home.js
@@ -2,7 +2,7 @@ function GetQueryString(name) {
 	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
 	var r = window.location.search.substr(1).match(reg);
 	if (r != null) return decodeURI(r[2]);
-	return "new_file.html";;
+	return "homegrid.html";;
 }
 
 $(document).ready(function() {
--- js/es-home.js
+++ js/es-home.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+	$.each(webs, function(n, value) {
+		var normalNode;
+		var group = value.g;
+		$("#navmain").append('<li class="navlicustomcss"><a href="#">' + value.n + '</a><ul id="' + value.g + '"></ul></li>');
+		$.each(value.web, function(n, value) {
+			if (value.u != undefined && value.hide == undefined)
+				$("#" + group).append('<li esOpenVaule="' + value.u + '"><a href="#">' + value.n + '</a></li>');
+		});
+	});
+
 	$("li").click(function() {
 		var urlframe = $(this).attr("esVaule");
 		var urlopen = $(this).attr("esOpenVaule");
@@ -11,25 +21,36 @@ $(document).ready(function() {
 			$(".menu .hover").addClass("current");
 		}
 	});
-	
-
 
 	$('a[rel*=leanModal]').leanModal({
 		top: 200,
 		closeButton: ".modal_close"
 	});
-	
+
+	/*
+	99Lime.com HTML KickStart by Joshua Gatcke
+	kickstart.js
+	*/
+	$('ul.menu').each(function(){
+		// add the menu toggle
+		$(this).prepend('<li class="menu-toggle"><a href="#"><span class="icon" data-icon="Y"></span> Menu</a></li>');
+
+		// find menu items with children.
+		$(this).find('li').has('ul').addClass('has-menu')
+		.find('a:first').append('<span class="arrow">&nbsp;</span>');
+	});
+	$('ul.menu li').hover(function(){
+		$(this).find('ul:first').stop(true, true).fadeIn('fast');
+		$(this).addClass('hover');
+	},		
+	function(){
+		$(this).find('ul').stop(true, true).fadeOut('slow');
+		$(this).removeClass('hover');
+	});
+	$('ul').find('li:first-child').addClass('first');
+	$('ul').find('li:last-child').addClass('last');
+
+
+
 	console.info("           ES           \n       做一个环保的人\n============================\nPowered by\nhttp://www.littleji.com\nhttp://www.xiaobaidonghui.cn\n\n In YanShan University 2015\n============================");
 });
-
-
-function SearchLibrary() {
-    var txtsjk = document.getElementById("wd").value;
-    var actionURL = "http://202.206.242.99/opac/openlink.php";
-//  var select1 = document.getElementById("select10");
-//  switch (select1.value) {
-        actionURL = actionURL + "?strSearchType=title&match_flag=forward&historyCount=1&strText=" + encodeURIComponent(txtsjk) + "&doctype=ALL&displaypg=20&showmode=list&sort=CATA_DATE&orderby=desc&location=ALL";
-//      break;
-//  }
-    window.open(actionURL, "_blank");
-}
